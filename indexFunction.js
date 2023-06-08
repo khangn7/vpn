3328be52bcff870a2ec41dce7a303fd4fa62f53a
Khang Ngo <1100057805@edu.cbe.ab.ca>
2023-06-08 10:25:40 -0600

    asdf

--- indexFunction.js
+++ indexFunction.js
@@ -1,1 +1,9 @@
-console.log("hello")
+const get_button = document.getElementById("get-button");
+const url_input = document.getElementById("url-input");
+
+const SERVER_URL = 0;
+
+function getURL(input_url)
+{
+    
+}
